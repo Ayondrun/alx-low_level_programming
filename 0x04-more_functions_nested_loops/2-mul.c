fdbcca76bf3a54b5901441c7948ceda28a66a8e4
Ayondrun <ayobamijoelobadare@gmail.com>
2022-07-07 08:37:49 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,16 +1,13 @@
 #include "main.h"
 
 /**
- *  mul -  checks for checks for a digit (0 through 9).
- *  @a: a -  Variable
- *  @b: b - variable
- *  Return: Always 0.
+ * mul - Multiplies two integers
+ * @a: The first number to multiply
+ * @b: The second number to multiply with first number
+ *
+ * Return: The result of multiplication between a param and b param
  */
 int mul(int a, int b)
 {
-	int mul;
-
-	mul = a * b;
-
-	return (mul);
+	return (a * b);
 }
